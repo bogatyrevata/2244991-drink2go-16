e99600cee89d45bbd76afd96bfd308ae75cdcab7
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2023-10-16 05:09:58 -0300

    добавила картинки, часть стилей, внесла изменения в разметку

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+const menu = () => {
+  let mainHeader = document.querySelector('.header');
+  let navToggle = document.querySelector('.header__toggle');
+
+  mainHeader.classList.remove('header--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (mainHeader.classList.contains('header--closed')) {
+      mainHeader.classList.remove('header--closed');
+      mainHeader.classList.add('header--opened');
+    } else {
+      mainHeader.classList.add('header--closed');
+      mainHeader.classList.remove('header--opened');
+    }
+  });
+};
+
+menu();
